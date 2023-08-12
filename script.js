8fa0197dc8378db13cb46760cea923976b6848b3
arya-ba01 <arya.ba00@gmail.com>
2023-08-12 15:11:47 +0330

    adding from structure in profile page and js script for signup/signin page number scroller

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
-const slider = document.querySelector('.special_offer_container');
-let isDown = false;
-let startX;
-let scrollLeft;
-
-slider.addEventListener('mouseDown', (e) => {
-    isDown = true;
-    slider.classList.add('active');
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
-});
-slider.addEventListener('mouseLeave', () => {
-    isDown = false;
-    slider.classList.remove('active')
-});
-slider.addEventListener('mouseUp', () => {
-    isDown = false;
-    slider.classList.remove('active')
-});
-slider.addEventListener('mouseMove', (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x =e.pageX - slider.offsetLeft;
-    const walk = (x -startX);
-    slider.scrollLeft = scrollLeft - walk;
-});
+$('input[type=number]').on('mousewheel', function(e) {
+    $(e.target).blur();
+  });
+  
